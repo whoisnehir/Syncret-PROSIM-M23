afc1de7ae52993a0167426d046e56e038822eaeb
whoisnehir <nehir.irey0@gmail.com>
2026-05-13 18:43:01 +0300

    Schimbari de design

--- SyncretSimulator/SyncretSimulator/MainForm.Designer.cs
+++ SyncretSimulator/SyncretSimulator/MainForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class MainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,27 +15,195 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pnlB1 = new System.Windows.Forms.Panel();
+            this.pnlB2 = new System.Windows.Forms.Panel();
+            this.pnlB3 = new System.Windows.Forms.Panel();
+            this.pnlB4 = new System.Windows.Forms.Panel();
+            this.chkS6 = new System.Windows.Forms.CheckBox();
+            this.chkS7 = new System.Windows.Forms.CheckBox();
+            this.chkS8 = new System.Windows.Forms.CheckBox();
+            this.btnS0 = new System.Windows.Forms.Button();
+            this.btnS1 = new System.Windows.Forms.Button();
+            this.btnS2 = new System.Windows.Forms.Button();
+            this.btnS3 = new System.Windows.Forms.Button();
+            this.btnS4 = new System.Windows.Forms.Button();
+            this.btnS5 = new System.Windows.Forms.Button();
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.lstEvents = new System.Windows.Forms.ListBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelB1 = new System.Windows.Forms.Label();
+            this.labelB2 = new System.Windows.Forms.Label();
+            this.labelB3 = new System.Windows.Forms.Label();
+            this.labelB4 = new System.Windows.Forms.Label();
+            this.SenzoriClapeta = new System.Windows.Forms.GroupBox();
+            this.pnlS6 = new System.Windows.Forms.Panel();
+            this.pnlS7 = new System.Windows.Forms.Panel();
+            this.pnlS8 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SenzoriClapeta.SuspendLayout();
             this.SuspendLayout();
             // 
-            // MainForm
+            // pnlB1, pnlB2, pnlB3, pnlB4 (Configurări locații)
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "MainForm";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.ResumeLayout(false);
+            this.pnlB1.Location = new System.Drawing.Point(193, 226);
+            this.pnlB1.Name = "pnlB1";
+            this.pnlB1.Size = new System.Drawing.Size(150, 30);
 
+            this.pnlB2.Location = new System.Drawing.Point(508, 226);
+            this.pnlB2.Name = "pnlB2";
+            this.pnlB2.Size = new System.Drawing.Size(150, 30);
+
+            this.pnlB3.Location = new System.Drawing.Point(199, 542);
+            this.pnlB3.Name = "pnlB3";
+            this.pnlB3.Size = new System.Drawing.Size(150, 30);
+
+            this.pnlB4.Location = new System.Drawing.Point(508, 542);
+            this.pnlB4.Name = "pnlB4";
+            this.pnlB4.Size = new System.Drawing.Size(150, 30);
+
+            // 
+            // Butoane (S0 - S5)
+            // 
+            this.btnS0.Location = new System.Drawing.Point(22, 134);
+            this.btnS0.Name = "btnS0";
+            this.btnS0.Size = new System.Drawing.Size(60, 60);
+            this.btnS0.Text = "S0";
+            this.btnS0.Click += new System.EventHandler(this.btnS0_Click);
+
+            this.btnS1.Location = new System.Drawing.Point(22, 226);
+            this.btnS1.Name = "btnS1";
+            this.btnS1.Size = new System.Drawing.Size(50, 50);
+            this.btnS1.Text = "S1";
+            this.btnS1.Click += new System.EventHandler(this.btnS1_Click);
+
+            this.btnS2.Location = new System.Drawing.Point(22, 322);
+            this.btnS2.Name = "btnS2";
+            this.btnS2.Size = new System.Drawing.Size(50, 50);
+            this.btnS2.Text = "S2";
+            this.btnS2.Click += new System.EventHandler(this.btnS2_Click);
+
+            this.btnS3.Location = new System.Drawing.Point(22, 422);
+            this.btnS3.Name = "btnS3";
+            this.btnS3.Size = new System.Drawing.Size(50, 50);
+            this.btnS3.Text = "S3";
+            this.btnS3.Click += new System.EventHandler(this.btnS3_Click);
+
+            this.btnS4.Location = new System.Drawing.Point(22, 528);
+            this.btnS4.Name = "btnS4";
+            this.btnS4.Size = new System.Drawing.Size(50, 50);
+            this.btnS4.Text = "S4";
+            this.btnS4.Click += new System.EventHandler(this.btnS4_Click);
+
+            this.btnS5.Location = new System.Drawing.Point(22, 626);
+            this.btnS5.Name = "btnS5";
+            this.btnS5.Size = new System.Drawing.Size(50, 50);
+            this.btnS5.Text = "S5";
+            this.btnS5.Click += new System.EventHandler(this.btnS5_Click);
+
+            // 
+            // Senzori și Log
+            // 
+            this.chkS6.Location = new System.Drawing.Point(20, 30);
+            this.chkS6.Name = "chkS6";
+            this.chkS6.Text = "S6 (L)";
+
+            this.chkS7.Location = new System.Drawing.Point(20, 60);
+            this.chkS7.Name = "chkS7";
+            this.chkS7.Text = "S7 (M)";
+
+            this.chkS8.Location = new System.Drawing.Point(20, 90);
+            this.chkS8.Name = "chkS8";
+            this.chkS8.Text = "S8 (R)";
+
+            this.lstEvents.Location = new System.Drawing.Point(772, 94);
+            this.lstEvents.Name = "lstEvents";
+            this.lstEvents.Size = new System.Drawing.Size(200, 400);
+
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 100;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+
+            // LED-urile (Panelurile mici)
+            this.pnlS6.Location = new System.Drawing.Point(199, 385);
+            this.pnlS6.Size = new System.Drawing.Size(30, 30);
+            this.pnlS6.Name = "pnlS6";
+
+            this.pnlS7.Location = new System.Drawing.Point(401, 385);
+            this.pnlS7.Size = new System.Drawing.Size(30, 30);
+            this.pnlS7.Name = "pnlS7";
+
+            this.pnlS8.Location = new System.Drawing.Point(606, 385);
+            this.pnlS8.Size = new System.Drawing.Size(30, 30);
+            this.pnlS8.Name = "pnlS8";
+
+            this.lblStatus.Location = new System.Drawing.Point(400, 20);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Text = "SYNCRET SYSTEM READY";
+
+            // 
+            // Adăugarea pe Form
+            // 
+            this.SenzoriClapeta.Location = new System.Drawing.Point(772, 551);
+            this.SenzoriClapeta.Size = new System.Drawing.Size(150, 130);
+            this.SenzoriClapeta.Controls.Add(this.chkS6);
+            this.SenzoriClapeta.Controls.Add(this.chkS7);
+            this.SenzoriClapeta.Controls.Add(this.chkS8);
+
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pnlB1);
+            this.Controls.Add(this.pnlB2);
+            this.Controls.Add(this.pnlB3);
+            this.Controls.Add(this.pnlB4);
+            this.Controls.Add(this.pnlS6);
+            this.Controls.Add(this.pnlS7);
+            this.Controls.Add(this.pnlS8);
+            this.Controls.Add(this.btnS0);
+            this.Controls.Add(this.btnS1);
+            this.Controls.Add(this.btnS2);
+            this.Controls.Add(this.btnS3);
+            this.Controls.Add(this.btnS4);
+            this.Controls.Add(this.btnS5);
+            this.Controls.Add(this.lstEvents);
+            this.Controls.Add(this.SenzoriClapeta);
+
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Name = "MainForm";
+            this.Text = "Syncret M23 Simulator";
+            this.SenzoriClapeta.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        // DECLARAȚIILE (Sunt vitale!)
+        private System.Windows.Forms.Panel pnlB1;
+        private System.Windows.Forms.Panel pnlB2;
+        private System.Windows.Forms.Panel pnlB3;
+        private System.Windows.Forms.Panel pnlB4;
+        private System.Windows.Forms.CheckBox chkS6;
+        private System.Windows.Forms.CheckBox chkS7;
+        private System.Windows.Forms.CheckBox chkS8;
+        private System.Windows.Forms.Button btnS0;
+        private System.Windows.Forms.Button btnS1;
+        private System.Windows.Forms.Button btnS2;
+        private System.Windows.Forms.Button btnS3;
+        private System.Windows.Forms.Button btnS4;
+        private System.Windows.Forms.Button btnS5;
+        private System.Windows.Forms.Label lblAlarm;
+        private System.Windows.Forms.ListBox lstEvents;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.GroupBox SenzoriClapeta;
+        private System.Windows.Forms.Panel pnlS6;
+        private System.Windows.Forms.Panel pnlS7;
+        private System.Windows.Forms.Panel pnlS8;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label labelB1;
+        private System.Windows.Forms.Label labelB2;
+        private System.Windows.Forms.Label labelB3;
+        private System.Windows.Forms.Label labelB4;
     }
 }
-
--- SyncretSimulator/SyncretSimulator/MainForm.cs
+++ SyncretSimulator/SyncretSimulator/MainForm.cs
@@ -1,25 +1,217 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace SyncretSimulator
 {
     public partial class MainForm : Form
     {
+        // --- STARE SISTEM (Variabile de proces) ---
+        private bool M1, M2, M3, M4; // Motoare Benzi
+        private bool isAlarmActive = false;
+        private int alarmTimerTicks = 0; // Pentru cele 5 secunde (50 tick-uri la 100ms)
+
         public MainForm()
         {
             InitializeComponent();
+            // Subscriere la evenimentul Load pentru a aplica stilul o singură dată
+            this.Load += (s, e) => 
+            {
+                ApplyIndustrialStyle();
+                // Configure timer after all controls are initialized
+                mainTimer.Interval = 100;
+                mainTimer.Enabled = true;
+            };
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        // --- LOGICA DE CONTROL (Executată la fiecare 100ms) ---
+        private void mainTimer_Tick(object sender, EventArgs e)
         {
+            ExecuteSafetyLogic();
+            ExecuteProcessLogic();
+            UpdateUI();
+        }
 
+        private void ExecuteSafetyLogic()
+        {
+            // 1. Verificare conflict senzori clapetă (S6, S7, S8)
+            int activeSensors = (chkS6.Checked ? 1 : 0) + (chkS7.Checked ? 1 : 0) + (chkS8.Checked ? 1 : 0);
+
+            if (activeSensors >= 2 && !isAlarmActive)
+            {
+                TriggerAlarm("EROARE: Conflict poziție clapetă!");
+            }
+        }
+
+        private void ExecuteProcessLogic()
+        {
+            if (isAlarmActive)
+            {
+                alarmTimerTicks--;
+                if (alarmTimerTicks <= 0)
+                {
+                    ResetAlarm();
+                }
+                return;
+            }
+
+            // --- REGULI LOGICE M23 (Interblocări) ---
+
+            // Regula B1: Pornește/Rămâne pornită doar dacă:
+            // (E pe ruta stânga S6 ȘI M3 merge) SAU (E pe ruta mijloc S7)
+            if (M1)
+            {
+                bool pathValid = (chkS6.Checked && M3) || chkS7.Checked;
+                if (!pathValid)
+                {
+                    M1 = false;
+                    LogAction("B1 Oprită: Rută invalidă.");
+                }
+            }
+
+            // Regula B2: Pornește/Rămâne pornită doar dacă:
+            // (E pe ruta dreapta S8 ȘI M4 merge) SAU (E pe ruta mijloc S7)
+            if (M2)
+            {
+                bool pathValid = (chkS8.Checked && M4) || chkS7.Checked;
+                if (!pathValid)
+                {
+                    M2 = false;
+                    LogAction("B2 Oprită: Rută invalidă.");
+                }
+            }
+
+            // Notă: M3 și M4 (ieșirile) nu au interblocări în documentație, 
+            // pot fi pornite/oprite liber de operator.
+        }
+
+        // --- EVENIMENTE BUTOANE (Comenzi Operator) ---
+        private void btnS1_Click(object sender, EventArgs e)
+        {
+            if (isAlarmActive) return;
+            // Verificăm condiția chiar la apăsare pentru feedback instant
+            if ((chkS6.Checked && M3) || chkS7.Checked) { M1 = true; LogAction("Start B1"); }
+            else { LogAction("Eroare: Condiții pornire B1 neîndeplinite."); }
+        }
+
+        private void btnS2_Click(object sender, EventArgs e)
+        {
+            if (isAlarmActive) return;
+            if ((chkS8.Checked && M4) || chkS7.Checked) { M2 = true; LogAction("Start B2"); }
+            else { LogAction("Eroare: Condiții pornire B2 neîndeplinite."); }
+        }
+
+        private void btnS3_Click(object sender, EventArgs e) { if (!isAlarmActive) { M3 = true; LogAction("Start B3 (Ieșire)"); } }
+        private void btnS4_Click(object sender, EventArgs e) { if (!isAlarmActive) { M4 = true; LogAction("Start B4 (Ieșire)"); } }
+
+        private void btnS0_Click(object sender, EventArgs e) // STOP GENERAL
+        {
+            StopAllMotors();
+            LogAction("!!! STOP GENERAL (S0) !!!");
+        }
+
+        private void btnS5_Click(object sender, EventArgs e) // STOP INTRĂRI
+        {
+            M1 = false;
+            M2 = false;
+            LogAction("Stop Intrări (S5)");
+        }
+
+        // --- METODE HELPER ---
+        private void StopAllMotors()
+        {
+            M1 = M2 = M3 = M4 = false;
+        }
+
+        private void TriggerAlarm(string message)
+        {
+            isAlarmActive = true;
+            alarmTimerTicks = 50; // 5 secunde
+            StopAllMotors();
+            lblStatus.Text = message;
+            lblStatus.ForeColor = Color.Red;
+            LogAction("ALARMĂ: " + message);
+        }
+
+        private void ResetAlarm()
+        {
+            isAlarmActive = false;
+            lblStatus.Text = "SYSTEM READY";
+            lblStatus.ForeColor = Color.White;
+        }
+
+        private void LogAction(string message)
+        {
+            if (lstEvents != null)
+                lstEvents.Items.Insert(0, $"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
+
+        private void UpdateUI()
+        {
+            // Actualizare culori benzi (Verde dacă merg, Gri dacă sunt oprite)
+            pnlB1.BackColor = M1 ? Color.LimeGreen : Color.FromArgb(60, 60, 60);
+            pnlB2.BackColor = M2 ? Color.LimeGreen : Color.FromArgb(60, 60, 60);
+            pnlB3.BackColor = M3 ? Color.LimeGreen : Color.FromArgb(60, 60, 60);
+            pnlB4.BackColor = M4 ? Color.LimeGreen : Color.FromArgb(60, 60, 60);
+
+            // Actualizare LED-uri senzori (Galben dacă e bifat, Gri dacă nu)
+            // Dacă e alarmă, facem "blink" cu roșu
+            if (isAlarmActive && alarmTimerTicks % 2 == 0)
+            {
+                pnlS6.BackColor = pnlS7.BackColor = pnlS8.BackColor = Color.Red;
+            }
+            else
+            {
+                pnlS6.BackColor = chkS6.Checked ? Color.Yellow : Color.DimGray;
+                pnlS7.BackColor = chkS7.Checked ? Color.Yellow : Color.DimGray;
+                pnlS8.BackColor = chkS8.Checked ? Color.Yellow : Color.DimGray;
+            }
+        }
+
+        // --- UI STYLING (Syncret Branding) ---
+        private void ApplyIndustrialStyle()
+        {
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            StyleLargePanel(pnlB1); StyleLargePanel(pnlB2);
+            StyleLargePanel(pnlB3); StyleLargePanel(pnlB4);
+            StyleSensorLED(pnlS6); StyleSensorLED(pnlS7); StyleSensorLED(pnlS8);
+            StyleButtonS0(btnS0);
+            StyleButtonS1S4(btnS1); StyleButtonS1S4(btnS2);
+            StyleButtonS1S4(btnS3); StyleButtonS1S4(btnS4);
+            StyleButtonS1S4(btnS5); // Am adăugat și S5
+            StyleStatusLabel(lblStatus);
+            btnS5.BackColor = Color.DarkOrange; // S5 e stop parțial, îl facem portocaliu
+            CheckBox[] sensors = { chkS6, chkS7, chkS8 };
+            foreach (var chk in sensors)
+            {
+                chk.ForeColor = Color.White;
+                chk.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+                // Dacă fundalul GroupBox-ului e prea închis:
+                chk.BackColor = Color.Transparent;
+            }
+
+            // Opțional: stilizează și GroupBox-ul care le conține
+            SenzoriClapeta.ForeColor = Color.LightGray;
+        }
+
+        private void StyleLargePanel(Panel p) { p.BackColor = Color.FromArgb(50, 50, 50); p.BorderStyle = BorderStyle.FixedSingle; }
+        private void StyleSensorLED(Panel p)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, p.Width, p.Height);
+            p.Region = new Region(path);
+        }
+        private void StyleButtonS0(Button b)
+        {
+            b.BackColor = Color.Crimson; b.FlatStyle = FlatStyle.Flat; b.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            GraphicsPath path = new GraphicsPath(); path.AddEllipse(0, 0, b.Width, b.Height); b.Region = new Region(path);
+        }
+        private void StyleButtonS1S4(Button b) { b.BackColor = Color.FromArgb(50, 150, 50); b.FlatStyle = FlatStyle.Flat; b.ForeColor = Color.White; }
+        private void StyleStatusLabel(Label l) { l.Dock = DockStyle.Top; l.Height = 50; l.TextAlign = ContentAlignment.MiddleCenter; l.ForeColor = Color.White; }
+        private void pnlS6_Paint(object sender, PaintEventArgs e)
+        {
+            // Nu scrie nimic aici. Această metodă există doar ca să dispară eroarea.
         }
     }
 }
